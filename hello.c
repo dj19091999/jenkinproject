d8696cb3d4057547958572e6a78bc6e88ac650df
Dheeraj jain <dheeraj4409.jain@gmail.com>
2020-06-11 21:38:02 +0530

    release

--- hello.c
+++ hello.c
@@ -3,10 +3,8 @@ int main()
 {
    int a=5;
    printf("Hello World\n");
-   printf("Hello mathura\n");
    printf(" hello everyone\n");
    printf("  release\n");
-   printf("  everyone\n");
    printf("%d",a);
    return 0;
 }
