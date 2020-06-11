b57e408477facaab83e3bc386c84814ec285e666
Dheeraj Jain <45208639+dj19091999@users.noreply.github.com>
2020-06-11 18:41:31 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -4,7 +4,8 @@ int main()
    printf("Hello World\n");
    printf("Hello mathura\n");
    printf("hello dheeraj\n");
-   printf("hello")
+   printf("hello\n");
+   printf("ddddd\n");
    printf("hello kosi\n");
    return 0;
 }
