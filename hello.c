0fd7a404291e2b0883b676784f2aeeec64a4021f
Dheeraj jain <dheeraj4409.jain@gmail.com>
2020-06-13 17:14:09 +0530

    newmessage

--- hello.c
+++ hello.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 int main()
 {
-   printf("Hello World\n");
    printf(" hello \n");
    printf(" qwertty \n");
-   printf("hello kosi\n")
    printf(" hello \n");
    return 0;
 }
