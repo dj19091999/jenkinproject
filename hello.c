130f885839e7869464228eb6693954db214580f8
Dheeraj jain <dheeraj4409.jain@gmail.com>
2020-06-13 16:52:55 +0530

    brelease

--- hello.c
+++ hello.c
@@ -3,6 +3,7 @@ int main()
 {
    printf("Hello World\n");
    printf(" hello \n");
+   printf(" qwertty \n");
    printf("hello kosi\n")
    printf(" hello \n");
    printf(" new with guithub branch \n");
